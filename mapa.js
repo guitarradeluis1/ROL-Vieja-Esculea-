02ee9a0e509a57f2b7f4a8da09ce7c46d5cc1ed7
Luis <46826849+guitarradeluis1@users.noreply.github.com>
2020-02-06 21:34:23 -0500

    Base funcional para juego de ron (Vieja escuela)

--- mapa.js
+++ mapa.js
@@ -1,27 +1,57 @@
 window.onload = function() {
+	document.getElementById('btn_new').onclick = function(){ obj.newJugador(); };
+	document.getElementById('btn_ene').onclick = function(){ obj.newEnemigo(); };
+	document.getElementById('btn_size').onclick = function(){ obj.chaengeSizeGamers(); };
 	obj.start();
 };
 var obj = {
 	canvas: 0,
 	ctx: 0,
-	sizeWidth: 850,
+	sizeWidth: 600,
 	sizeHeight: 400,
+	sizeGamers: 20,
+	sizeText: 25,
 	centerx: 0,
 	centery: 0,
 	jugadores: [],
 	npc: [],
+	selecion: 0,
+	tempPosicion: {x:20 , y:20},
 	start: ()=>{
-		var { jugadores }= obj;
+		const { sizeWidth, sizeHeight, getMousePos, tempPosicion } = obj;
+		var divCanvas = document.getElementById("divCanvas");
+		divCanvas.style.width = `${sizeWidth}px`;
+		divCanvas.style.height = `${sizeHeight}px`;
+		var canvasHtml = `<canvas id="canvas" width="${sizeWidth}" height="${sizeHeight}">Tu navegador no soporta html5...</canvas>`;
+		divCanvas.innerHTML = canvasHtml;
 		obj.canvas = document.getElementById("canvas");
 		canvas.ctx = obj.canvas.getContext("2d");
+		canvas.addEventListener('click', function(evt) {
+			var mousePos = getMousePos(canvas, evt);
+			tempPosicion.x = mousePos.x;
+			tempPosicion.y = mousePos.y;
+			document.getElementById("posicionEjemplo").innerHTML = `${mousePos.x}, ${mousePos.y}`;
+			/*
+			var uno = true;
+			jugadores.map(j=>{
+				if(j.id = obj.selecion){
+					if(uno){
+						j.setPosition(mousePos.x, mousePos.y);
+						console.log(j.id);
+						uno = false;
+						reload();
+					}
+				}
+			});*/
+		}, false);
 		obj.reload();
-		/*
-		document.getElementById('btnNewP').click = function(){
-			var jugador = new Jugador('luis', 'Amudiel', true);
-			jugadores.push(jugador);
-			jugador.paint('datosJugador');
-			jugador.paint('datosNpc');
-		};*/
+	},
+	getMousePos: (canvas, evt)=>{
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
 	},
 	imagen: ()=>{
 		const { sizeWidth, sizeHeight } = obj;
@@ -29,8 +59,194 @@ var obj = {
 		fondo.src = './mapa1.jpg'; 
 		canvas.ctx.drawImage(fondo, 0, 0, sizeWidth, sizeHeight);
 	},
+	placeMap: ()=>{
+		const { sizeGamers, sizeText, jugadores } = obj;
+		jugadores.map(j=>{
+			canvas.ctx.fillStyle = `black`;
+			canvas.ctx.font = `${sizeText}px Arial`;
+			canvas.ctx.fillText(`${j.personaje}`, j.x, j.y -2);
+			canvas.ctx.fillStyle = `${j.color}`;
+			canvas.ctx.fillRect(j.x, j.y, sizeGamers, sizeGamers);
+			canvas.ctx.beginPath();
+		});
+	},
 	reload: ()=>{
-	const { line, origin, destiny, sizeIcon, imagen } = obj;
+		const { imagen, placeMap } = obj;
 		imagen();
+		placeMap();
+	},
+	chaengeSizeGamers:()=>{
+		const { reload } = obj;
+		var tmp = prompt("Tamaño de personajes en el mapa (10, 20....):", "");
+		obj.sizeGamers = parseInt(tmp);
+		var tmp = prompt("Tamaño del texto (10, 20....):", "");
+		obj.sizeText = parseInt(tmp);
+		reload();
+	},
+	newJugador: ()=>{
+		var { jugadores, list }= obj;
+		var jugador = new Jugador(true);
+		jugador.color = document.getElementById('color').value;
+		jugadores.push(jugador);
+		list();
+	},
+	newEnemigo: ()=>{
+		var { jugadores, listNpc }= obj;
+		var jugador = new Jugador(false);
+		jugador.color = document.getElementById('color').value;
+		jugadores.push(jugador);
+		listNpc();
+	},
+	list: ()=>{
+		const { jugadores, reload, painData } = obj;
+		var lista = document.getElementById('lista');
+		lista.innerHTML = "";
+		var ol = document.createElement("ol");
+		jugadores.map(j=>{
+			if(j.live){
+				if(j.jugador){
+					var li = document.createElement("li");
+					li.setAttribute("id", j.id);
+					li.innerHTML = `
+						<b>${j.personaje}</b> ${j.nombre}<br/>
+						PV:<b>${j.pv()}</b><br/>
+						ORO:<b>${j.oro}</b><br/>
+						NV:<b>${j.nivel} (${j.experiencia} Exp)</b><br/>
+					`;
+					li.onclick = function(){ 
+						obj.selecion = j.id;
+						painData(j.id, true); 
+					}
+					li.style.cursor = "pointer";
+					li.style.color = `${j.color}`;
+					ol.appendChild(li);
+				}
+			}
+		});
+		lista.appendChild(ol);
+		reload();
+	},
+	listNpc: ()=>{
+		const { jugadores, reload, painData } = obj;
+		var lista = document.getElementById('listaNpc');
+		lista.innerHTML = "";
+		var ol = document.createElement("ol");
+		jugadores.map(j=>{
+			if(j.live){
+				if(j.jugador == false){
+					var li = document.createElement("li");
+					li.setAttribute("id", j.id);
+					li.innerHTML = `
+						<b>${j.personaje}</b> ${j.nombre}<br/>
+						PV:<b>${j.pv()}</b><br/>
+						ORO:<b>${j.oro}</b><br/>
+						NV:<b>${j.nivel} (${j.experiencia} Exp)</b><br/>
+					`;
+					li.onclick = function(){ 
+						obj.selecion = j.id;
+						painData(j.id, false); 
+					}
+					li.style.cursor = "pointer";
+					li.style.color = `${j.color}`;
+					ol.appendChild(li);
+				}
+			}
+		});
+		lista.appendChild(ol);
+		reload();
+	},
+	painData: (id, tipo)=>{
+		const { jugadores, painData, list, reload, tempPosicion } = obj;
+		var div = document.getElementById('datosJugador');
+		var data = jugadores.filter(j=> j.id == id);
+		data = data[0];
+		if(tipo){
+			var nombreRaza = data.raza.filter(ra => ra.id == data.razaId);
+			var nombreClase = data.clase.filter(cl => cl.id == data.claseId);
+		}
+		var html = `
+		<table>
+			<tr>
+				<td>
+					Nombre: <b>${data.personaje}</b> (${data.nombre})</b><br/>`;
+		if(tipo){
+			html += `
+					Raza: <b>${nombreRaza[0].nombre} (MOV ${nombreRaza[0].mov})</b><br/>
+					Clase: <b>${nombreClase[0].nombre} (DA ${nombreClase[0].da})</b><br/>`;
+		}else{
+			html += `PV: <b>${data.vida})</b><br/>`;
+
+		}
+			html += `
+					<div id="detalleJugador"></div>
+				</td>
+				<td>
+					<b>Datos:</b><br/><ol id="detallesDatos"></ol>
+				</td>
+				<td>
+					<b>Habilidades:</b><br/><ol id="detallesHabilidad"></ol>
+				</td>
+		</tr>`;
+		html += `</table>`;
+		div.innerHTML = html;
+		var detalleJugador = document.getElementById('detalleJugador');
+		var oro = document.createElement('span');
+		oro.innerHTML = "<b>Oro</b> |";
+		oro.style.cursor = "pointer";
+		oro.onclick = function(){
+			data.setOro();
+			painData(id);
+			list();
+		};
+		detalleJugador.appendChild(oro);
+		var exp = document.createElement('span');
+		exp.innerHTML = "<b>Exp</b> |";
+		exp.style.cursor = "pointer";
+		exp.onclick = function(){
+			data.setExp();
+			reload();
+			list();
+		};
+		detalleJugador.appendChild(exp);
+		var del = document.createElement('span');
+		del.innerHTML = "<b>Posicion</b> |";
+		del.style.cursor = "pointer";
+		del.onclick = function(){
+			data.setPosition(tempPosicion.x, tempPosicion.y);
+			painData(id);
+			list();
+		};
+		detalleJugador.appendChild(del);
+		var del = document.createElement('span');
+		del.innerHTML = "<b>Eliminar</b> |";
+		del.style.cursor = "pointer";
+		del.onclick = function(){
+			data.eliminar();
+			painData(id);
+			list();
+		};
+		detalleJugador.appendChild(del);
+		var detalles = document.getElementById('detallesDatos');
+		data.datos.map(ha=>{
+			var li = document.createElement('li');
+			li.style.cursor = "pointer";
+			li.innerHTML = `${ha.pref} (+${ha.puntos})`;
+			li.onclick = function(){ 
+				data.setDatos(`${ha.nombre}`);
+				painData(id);
+			};
+			detalles.appendChild(li);
+		});
+		var detalles = document.getElementById('detallesHabilidad');
+		data.habilidades.map(ha=>{
+			var li = document.createElement('li');
+			li.style.cursor = "pointer";
+			li.innerHTML = `${ha.nombre} (+${ha.puntos})`;
+			li.onclick = function(){ 
+				data.setHabilidades(`${ha.nombre}`);
+				painData(id);
+			};
+			detalles.appendChild(li);
+		});
 	}
 };
--- personajes.js
+++ personajes.js
@@ -1,71 +1,171 @@
-
 /**
  * sistema base para personajes o NPC
  * @description Solo busca crear una instancia facil de editar
- * @param {string} nombre
- * @param {string} personaje
  * @param {string} tipo (true = jugador | false = NPC)
  */
-var Jugador = function(nombre, personaje, tipo){
+var Jugador = function(tipo){
+	this.id = ((new Date()).getTime() * parseInt((Math.random()*1000)));
+	this.live = true;
 	this.x = 20;
 	this.y = 20;
-	this.color = "#F25252";
+	this.oro = 0;
 	this.jugador = tipo;
-	this.nombre = nombre;
-	this.personaje = personaje;
+	this.nombre = tipo? prompt("Personaje:", ""): '';
+	this.personaje = prompt("Nombre:", "");
+	this.nivel = 0;
 	this.experiencia = 0;
-	this.raza = '';
-	this.clase = '';
+	this.razaId = tipo? prompt("Raza [0: Enano, 1: Mediano, 2: Humano, 3: Elfo]", ""): null;
+	this.raza = [
+		{id: 0, nombre:'Enano', mov: 9},
+		{id: 1, nombre:'Mediano', mov: 9},
+		{id: 2, nombre:'Humano', mov: 12},
+		{id: 3, nombre:'Elfo', mov: 12},
+	];
+	this.claseId = tipo? prompt("Clase [0: Guerrero, 1: Hechicero, 2: Bribon]", ""): null;
+	this.clase = [
+		{id: 0, nombre:'Guerrero', da: 8},
+		{id: 1, nombre:'Hechicero', da: 4},
+		{id: 2, nombre:'Bribon', da: 6},
+	];
 	this.talentos = [];
 	this.transfondos = [];
 	this.habilidades = [
-		{nombre:'lerta', puntos: 0},
-		{nombre:'comunicacion', puntos: 0},
-		{nombre:'manipulacion', puntos: 0},
-		{nombre:'erudicion', puntos: 0},
-		{nombre:'subterfugio', puntos: 0},
-		{nombre:'supervivencia', puntos: 0},
+		{nombre:'Alerta', puntos: 0},
+		{nombre:'Comunicacion', puntos: 0},
+		{nombre:'Manipulacion', puntos: 0},
+		{nombre:'Erudicion', puntos: 0},
+		{nombre:'Subterfugio', puntos: 0},
+		{nombre:'Supervivencia', puntos: 0},
 	];
 	this.datos = [
-		{nombre:'fuerza', pref:'FU', puntos: 0},
-		{nombre:'destreZa', pref:'DES', puntos: 0},
-		{nombre:'constitucion', pref:'CON', puntos: 0},
-		{nombre:'inteligencia', pref:'INT', puntos: 0},
-		{nombre:'sabiduria', pref:'SAB', puntos: 0},
-		{nombre:'carisma', pref:'CAR', puntos: 0},
+		{nombre:'Fuerza', pref:'FU', puntos: 0},
+		{nombre:'Destreza', pref:'DES', puntos: 0},
+		{nombre:'Constitucion', pref:'CON', puntos: 0},
+		{nombre:'Inteligencia', pref:'INT', puntos: 0},
+		{nombre:'Sabiduria', pref:'SAB', puntos: 0},
+		{nombre:'Carisma', pref:'CAR', puntos: 0},
 	];
+	//this.imagen = prompt("Imagen:", "");
+	this.color =  "";//prompt("Color (ingles):", "");
+	this.vida = tipo? 0: prompt("Vida", "");
 };
 
 /**
- * @description Miestra html de sus atributos in to div html
+ * @description Desavilitar personaje
+ */
+Jugador.prototype.eliminar = function(){
+	if (confirm(`Seguro quiere eliminar a ${this.personaje}`)) {
+		this.live = false;
+	} else {
+	}
+}
+
+/**
+ * @description Cambia cantidad de oro
+ */
+Jugador.prototype.setOro = function(){
+	var tmp = prompt(`Oro (${this.oro})`, `${this.oro}`);
+	this.oro = parseInt(tmp);
+}
+
+/**
+ * @description Cambia cantidad de experiencia y suvida de niles (10 = + 1NV)
+ */
+Jugador.prototype.setExp = function(){
+	var tmp = prompt(`NV/Exp (${this.nivel}/${this.experiencia})`, ``);
+	this.experiencia = this.experiencia + parseInt(tmp);
+	if(this.experiencia >= 10){
+		this.experiencia = this.experiencia - 10;
+		this.nivel = this.nivel + 1;
+		alert(`<b>${this.personaje}</b> subió al nivel ${this.nivel}`);
+	}
+}
+
+/**
+ * @description Cambia la posicion que ubica (canvas)
+ * @param {float} x
+ * @param {float} y
+ */
+Jugador.prototype.setPosition = function(x, y){
+	//var tmp = prompt(`X:`, `${this.x}`);
+	this.x = parseFloat(x);
+	//var tmp = prompt(`Y:`, `${this.y}`);
+	this.y = parseFloat(y);
+}
+
+/**
+ * @description Cambia datos acorde a su nombre
  * @param {string} nombre
  */
-Jugador.prototype.paint = function(id){
-	let obj = this;
-	const div = document.getElementById(id);
-	/*
-	var tabla = document.createElement("table");
-	var hilera = document.createElement("tr");
-	var celda = document.createElement("td");
-	celda.appendChild( document.createTextNode("texto") );
-	hilera.appendChild(celda);
-	tabla.appendChild(hilera);*/
-	div.innerHTML = `
-	<table>
-		<tr>
-			<td><b>Nombre</b></td>
-			<td>${obj.personaje} (${obj.nombre})</td>
-		</tr>
-		<tr>
-			<td><b>Raza</b></td>
-			<td>${obj.raza}</td>
-		</tr>
-		<tr>
-			<td><b>Clase</b></td>
-			<td>${obj.clase}</td>
-		</tr>
-	</table>`;
-};
+Jugador.prototype.setDatos = function(nombre){
+	this.datos.map(ha=>{
+		if(ha.nombre == nombre){
+			var tmp = prompt(`${ha.nombre}`, `${ha.puntos}`);
+			ha.puntos = parseInt(tmp);
+		}
+	});
+}
+
+/**
+ * @description Cambia habilidad acorde a su nombre
+ * @param {string} nombre
+ */
+Jugador.prototype.setHabilidades = function(nombre){
+	this.habilidades.map(ha=>{
+		if(ha.nombre == nombre){
+			var tmp = prompt(`${ha.nombre}`, `${ha.puntos}`);
+			ha.puntos = parseInt(tmp);
+		}
+	});
+}
+
+/**
+ * @returns {number} vida PV
+ */
+Jugador.prototype.pv = function(id){
+	//let obj = this;
+	return this.id;
+}
+
+/**
+ * @returns {number} movimiento MOV
+ */
+Jugador.prototype.mov = function(id){
+	//let obj = this;
+	return this.id;
+}
+
+/**
+ * @returns {number} defensa DEF
+ */
+Jugador.prototype.def = function(id){
+	//let obj = this;
+	return this.id;
+}
+
+/**
+ * @returns {number} ataque ATQ
+ */
+Jugador.prototype.atq = function(id){
+	//let obj = this;
+	return this.id;
+}
+
+/**
+ * @returns {number} instinto INS
+ */
+Jugador.prototype.ins = function(id){
+	//let obj = this;
+	return this.id;
+}
+
+/**
+ * @returns {number} poder POD
+ */
+Jugador.prototype.pod = function(id){
+	//let obj = this;
+	return this.id;
+}
 //var opcion = prompt("Introduzca su nombre:", "Aner Barrena")
 /*
 var txt;
